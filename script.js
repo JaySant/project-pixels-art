921e0bf2885aec3ca09af83c6abcaeceb7d53ba4
Jay Santos <jaysant15@gmail.com>
2022-01-08 16:46:05 -0300

    Requisito 7 - Ao ser clicado add a classe selected e remove o que foi selecionado

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 
  // Location
 window.onload = function(){
-document.getElementById('black').classList.add('selected')
-}
+ document.getElementById('black').classList.add('selected')
+ }
 
-   
+// Aqui diz qual cor foi clicada 
+let colorPalette = document.querySelector('#color-palette')
+colorPalette.addEventListener('click', event => {
+    let colorClicked = event.target 
+    let colorSelected = document.querySelector('.selected')
+        if(colorClicked !== colorSelected){
+            colorClicked.classList.add('selected')
+            colorSelected.classList.remove('selected')
+        }
         
-   
+        
+    })
+// Obrigada PatriciaDivi por ter explicado o target na monitoria e me dado uma luz ao meu problema com escutador
+ 
