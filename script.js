8c609d7399ffd1e9fa308a5d7edaf94c651416d3
Jay Santos <jaysant15@gmail.com>
2022-01-10 07:06:15 -0300

    Requisito 7 - Add butao com funçao de remover

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 
- // Location
-window.onload = function(){
+ // Selected default --- Aqui implementa a cor preta com o selected por padrão assim que a pagina é iniciada 
+ 
+ window.onload = function(){
  document.getElementById('black').classList.add('selected')
  }
 
-// Aqui diz qual cor foi clicada 
-let colorPalette = document.querySelector('#color-palette')
-colorPalette.addEventListener('click', event => {
-    let colorClicked = event.target 
-    let colorSelected = document.querySelector('.selected')
-        if(colorClicked !== colorSelected){
-            colorClicked.classList.add('selected')
-            colorSelected.classList.remove('selected')
-        }
-        
+// Clicar --- Aqui diz qual cor foi clicada e remove o que já foi selecionado
+// Obrigada PatriciaDivi por ter explicado o target na monitoria e me dado uma luz ao meu problema com escutador
+
+    let colorPalette = document.querySelector('#color-palette')
+    colorPalette.addEventListener('click', event => {
+        let colorClicked = event.target 
+        let colorSelected = document.querySelector('.selected')
+            if(colorClicked !== colorSelected){
+                colorClicked.classList.add('selected')
+                colorSelected.classList.remove('selected')
+                
+                
+            }
         
     })
-// Obrigada PatriciaDivi por ter explicado o target na monitoria e me dado uma luz ao meu problema com escutador
- 
+
+ // Adiciona a classe selected
+
+
+function apagar(){
+    let pixel = document.getElementsByClassName('pixel')
+    for(i = 0; i < pixel.length; i += 1){
+        pixel[i].style.background = 'white'
+     }
+}
+
+
+
+                                        
+    
+
