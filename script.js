b0ecc23b372b770cc1c0090c18ad987f2914e3a6
Jay Santos <jaysant15@gmail.com>
2022-01-31 13:37:46 -0300

    Requisito 8 e 9

--- script.js
+++ script.js
@@ -1,36 +1,66 @@
+let colorPalette = document.querySelector('#color-palette')
 
- // Selected default --- Aqui implementa a cor preta com o selected por padrão assim que a pagina é iniciada 
+// Selected default --- Aqui implementa a cor preta com o selected por padrão assim que a pagina é iniciada 
  
- window.onload = function(){
- document.getElementById('black').classList.add('selected')
+window.onload = function(){
+    let cor = document.getElementsByClassName('color')
+    cor[0].style.backgroundColor = 'black'
+    cor[1].style.backgroundColor = 'red'
+    cor[2].style.backgroundColor = 'blue'
+    cor[3].style.backgroundColor = 'yellow'
+    cor[0].classList.add('selected')
  }
 
 // Clicar --- Aqui diz qual cor foi clicada e remove o que já foi selecionado
 // Obrigada PatriciaDivi por ter explicado o target na monitoria e me dado uma luz ao meu problema com escutador
 
-    let colorPalette = document.querySelector('#color-palette')
+    
     colorPalette.addEventListener('click', event => {
         let colorClicked = event.target 
         let colorSelected = document.querySelector('.selected')
-            if(colorClicked !== colorSelected){
+        if(colorClicked !== colorSelected){
                 colorClicked.classList.add('selected')
                 colorSelected.classList.remove('selected')
-                
-                
             }
-        
     })
 
- // Adiciona a classe selected
 
+    function apagar(){
+        let pixel = document.getElementsByClassName('pixel')
+        for(i = 0; i < pixel.length; i += 1){
+            pixel[i].style.background = 'white'
+         }
+    }
+    
+ let pixelBoard = document.querySelector('#pixel-board')
+ pixelBoard.addEventListener('click', event => {
+       let seleColor = document.querySelector('.selected').style.backgroundColor;
+       let colorClick = event.target
+        if(colorClick.classList.contains('pixel')){
+            colorClick.style.backgroundColor = seleColor 
+        } 
+     })
+    
+    
 
-function apagar(){
-    let pixel = document.getElementsByClassName('pixel')
-    for(i = 0; i < pixel.length; i += 1){
-        pixel[i].style.background = 'white'
-     }
+for(let index = 1; index <= 5; index += 1){
+    let divName = document.createElement('div')
+    divName.id = `line${index}`
+    pixelBoard.appendChild(divName)
+    const lineBoard = document.getElementById(`line${index}`) 
+    for(let index = 1; index <= 5; index += 1){
+        let divName = document.createElement('div')
+        divName.className = `column${index} pixel`
+        divName.style.display = 'inline-block'
+        divName.style.width = '40px'
+        divName.style.height = '40px'
+        divName.style.backgroundColor = 'white'
+        divName.style.border = '1px solid black'
+        lineBoard.appendChild(divName)
+
+    }
 }
-
+     
 
 
                                         
